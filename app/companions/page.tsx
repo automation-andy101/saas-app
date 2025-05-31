46b7370195126ddb364a1e47e1504b665977d106
automation.andy101 <automation.andy101@gmail.com>
2025-05-31 07:33:39 +0100

    Started adding CompanionLibrary component

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import { getAllCompanions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const page = () => {
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+  const companions = await getAllCompanions({ subject, topic });
+  
   return (
-    <div>Companions Page</div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">Filters</div>
+      </section>
+      
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard 
+            key={companion.id} 
+            {...companion} 
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
   )
 }
 
-export default page
+export default CompanionsLibrary
